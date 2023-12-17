215424ad729ad4bcdde663b984b160691a439133
Nynence <rishabkrn@gmail.com>
2023-12-18 04:05:23 +1100

    imp

--- colourmode.js
+++ colourmode.js
@@ -21,6 +21,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const colorPickerBtn = document.getElementById('colorPickerBtn');
 const accentColorPicker = document.getElementById('accentColorPicker');
+const colorDisplay = document.getElementById('colorDisplay');
+
+// Display current accent color
+colorDisplay.textContent = getComputedStyle(document.documentElement).getPropertyValue('--accent-color');
 
 colorPickerBtn.addEventListener('click', () => {
     accentColorPicker.click();
@@ -29,4 +33,13 @@ colorPickerBtn.addEventListener('click', () => {
 accentColorPicker.addEventListener('input', (event) => {
     const newAccentColor = event.target.value;
     document.documentElement.style.setProperty('--accent-color', newAccentColor);
+    colorDisplay.textContent = newAccentColor;
+});
+
+// Close the color picker on clicking outside
+document.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target !== accentColorPicker && target !== colorPickerBtn) {
+        accentColorPicker.removeAttribute('open');
+    }
 });
