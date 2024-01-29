3353adef4a20b5f1a897a8ffa3bc6be80061e485
Nynence <rishabkrn@gmail.com>
2024-01-30 02:52:39 +1100

    responsiveness

--- fullscreen.js
+++ fullscreen.js
@@ -62,49 +62,28 @@ sliderContainer.addEventListener('mouseup', () => {
 //web share demo
 
 
-// const shareButton = document.querySelector('.sharetitle');
-// const shareDialog = document.querySelector('#shareDialog');
-// // const closeButton = document.querySelector('.close-button');
-
-// shareButton.addEventListener('click', event => {
-//     if (navigator.share) {
-//         navigator.share({
-//             title: 'WebShare API Demo',
-//             url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
-//         }).then(() => {
-//             console.log('Thanks for sharing!');
-//         })
-//         .catch(console.error);
-//     } else {
-//         shareDialog.classList.add('is-open');
-//     }
-// });
-
-// closeButton.addEventListener('click', event => {
-//     shareDialog.classList.remove('is-open');
-// });
-
-
-
-
-const shareButton = document.querySelector('.share-button');
-const shareDialog = document.querySelector('.share-dialog');
-const closeButton = document.querySelector('.close-button');
+const shareButton = document.querySelector('.sharetitle');
+const shareDialog = document.querySelector('#shareDialog');
+// const closeButton = document.querySelector('.close-button');
 
 shareButton.addEventListener('click', event => {
-  if (navigator.share) { 
-   navigator.share({
-      title: 'WebShare API Demo',
-      url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
-    }).then(() => {
-      console.log('Thanks for sharing!');
-    })
-    .catch(console.error);
+    if (navigator.share) {
+        navigator.share({
+            title: 'WebShare API Demo',
+            url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
+        }).then(() => {
+            console.log('Thanks for sharing!');
+        })
+        .catch(console.error);
     } else {
         shareDialog.classList.add('is-open');
     }
 });
 
 closeButton.addEventListener('click', event => {
-  shareDialog.classList.remove('is-open');
+    shareDialog.classList.remove('is-open');
 });
+
+
+
+
