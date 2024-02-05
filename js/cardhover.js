211890267719d8b4f90850a5a00a00418f8d67c5
Nynence <rishabkrn@gmail.com>
2024-02-06 00:03:44 +1100

    cursor update and restructure

---
+++ js/cardhover.js
@@ -0,0 +1,117 @@
+var card = document.getElementById('card');
+var mouseHover = false;
+var mousePosition = { x: 0, y: 0 };
+var cardSize = { width: 0, height: 0 };
+var SCALE_X = 4;
+var SCALE_Y = 8;
+
+function handleMouseMove(e) {
+  if (!mouseHover) return;
+  var rect = card.getBoundingClientRect();
+  var x = e.clientX - rect.left;
+  var y = e.clientY - rect.top;
+  mousePosition = { x, y };
+  cardSize = {
+    width: card.offsetWidth || 0,
+    height: card.offsetHeight || 0,
+  };
+  card.style.transform = `perspective(1000px) rotateX(${
+    (mousePosition.y / cardSize.height) * -(SCALE_Y * 2) + SCALE_Y
+  }deg) rotateY(${
+    (mousePosition.x / cardSize.width) * (SCALE_X * 2) - SCALE_X
+  }deg) translateZ(10px)`;
+}
+
+function handleMouseOut() {
+  mouseHover = false;
+  card.style.transform = 'perspective(600px) rotateX(0deg) rotateY(0deg) translateZ(0px)';
+}
+
+function handleMouseOver() {
+  mouseHover = true;
+}
+
+if (window.innerWidth > 1024) {
+  // Add event listeners only for screens wider than 1024 pixels
+  card.addEventListener('mousemove', handleMouseMove);
+  card.addEventListener('mouseout', handleMouseOut);
+  card.addEventListener('mouseover', handleMouseOver);
+}
+
+
+// cursor
+
+document.addEventListener("DOMContentLoaded", function() {
+  const customCursor = document.querySelector('.custom-cursor');
+  const cursorHalfWidth = 30 / 2;
+  const cursorHalfHeight = 23 / 2;
+
+  document.addEventListener("mousemove", function(event) {
+    const delay = 30;
+    setTimeout(function() {
+      const x = event.clientX - cursorHalfWidth;
+      const y = event.clientY - cursorHalfHeight;
+      customCursor.style.transform = `translate(${x}px, ${y}px) ${customCursor.classList.contains('pointer-active') ? 'scale(1.5)' : 'scale(1)'}`;
+    }, delay);
+  });
+
+  const sidebar = document.getElementById('sidebar');
+  const homePage = document.querySelector('.home-page');
+  const clickableElements = document.querySelectorAll('.pointer,  .zoomD');
+  const hiddenElements = document.querySelectorAll('.backforward, .colourbtns, .resize-handle, #searchInput' );
+
+  let hideTimeout;
+
+  const hideCursor = () => {
+    clearTimeout(hideTimeout);
+    hideTimeout = setTimeout(() => {
+      customCursor.style.opacity = '0';
+    }, 300);
+  };
+
+  const showCursor = () => {
+    clearTimeout(hideTimeout);
+    customCursor.style.opacity = '1';
+  };
+
+
+  hiddenElements.forEach(hider => {
+    hider.addEventListener('mouseenter', () => {
+      customCursor.style.display = 'none';
+
+
+    });
+    hider.addEventListener('mouseleave', () => {
+      customCursor.style.display = 'block';
+
+     
+    });
+  });
+
+
+
+
+  clickableElements.forEach(element => {
+    element.addEventListener('mouseenter', () => {
+      // customCursor.style.display = 'none';
+
+      customCursor.classList.add('pointer-active');
+      customCursor.style.transform = `translate(${x}px, ${y}px) scale(1)`;
+    });
+    element.addEventListener('mouseleave', () => {
+      customCursor.classList.remove('pointer-active');
+      customCursor.style.transform = `translate(${x}px, ${y}px) scale(1)`;
+    });
+  });
+
+  sidebar.addEventListener('mouseleave', hideCursor);
+  homePage.addEventListener('mouseleave', hideCursor);
+
+  sidebar.addEventListener('mouseenter', showCursor);
+  homePage.addEventListener('mouseenter', showCursor);
+
+  if (document.body.classList.contains('.item')) {
+    customCursor.style.display = 'none';
+  }
+});
+document.body.style.overflow = 'hidden';
---
+++ js/meswap.js
@@ -0,0 +1,18 @@
+document.getElementById('p').style.display = 'block';
+
+function toggleContent() {
+    var h4 = document.getElementById('toggleText');
+    var p = document.getElementById('p');
+    var p2 = document.getElementById('p2');
+
+    // Toggle the visibility of paragraphs
+    if (p.style.display === 'block') {
+        p.style.display = 'none';
+        p2.style.display = 'block';
+        h4.classList.add('highlight');
+    } else {
+        p.style.display = 'block';
+        p2.style.display = 'none';
+        h4.classList.remove('highlight');
+    }
+}
