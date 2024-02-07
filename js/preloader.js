803f8797c1dd7eca68e18bba535bbf4aea984739
Nynence <rishabkrn@gmail.com>
2024-02-07 20:15:59 +1100

    preloader

---
+++ js/preloader.js
@@ -0,0 +1,11 @@
+// script.js
+window.addEventListener("load", function () {
+    const loader = document.getElementById("preloader");
+    const content = document.getElementById("content");
+
+    // Hide preloader
+    loader.style.display = "none";
+
+    // Show content
+    content.style.display = "block";
+});
