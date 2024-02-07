a2172438128d5f7229cbc3a5c182a626db8af5a7
Nynence <rishabkrn@gmail.com>
2024-02-07 22:34:49 +1100

    prehidden

--- js/preloader.js
+++ js/preloader.js
@@ -1,7 +1,8 @@
-// script.js
 window.addEventListener("load", function () {
     const content = document.getElementById("content");
 
     // Show content
     content.style.visibility = "visible";
 });
+
+
