0d4bb226ecff627fea7988da3b1bbbb8aecf574f
Nynence <rishabkrn@gmail.com>
2024-04-20 00:30:17 +1000

    index fix

--- projects/sidebarp.php
+++ projects/sidebarp.php
@@ -35,7 +35,7 @@
 							
 						</li>
 						<li id="homeItem2" class="paddinghome">
-							<a href="/index.php" class="home2" >
+							<a href="/index.html" class="home2" >
 								<svg data-encore-id="icon" role="img" aria-hidden="true" class="home-active" viewBox="0 0 24 24">
 									<path  d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V7.577a2 2 0 0 0-1-1.732l-7.5-4.33z"></path>
 								</svg>
